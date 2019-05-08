b03c80642e6cb406e97fa1609c0d5e9ef20d6812
Charles Eric LaForest <eric.laforest@gmail.com>
2019-05-08 02:44:07 +0100

    Faster display, OI creation bugfix, lower gain and range on SRF08s

--- robot_status.py
+++ robot_status.py
@@ -10,31 +10,35 @@ def robot_status(OI, SRL, SRR):
     SRL.do_ranging()
     SRL.read_light()
     SRL.read_ranges()
-    print("SRL light: {0} Ranges: {1}".format(SRL.light, SRL.ranges))
 
     SRR.do_ranging()
     SRR.read_light()
     SRR.read_ranges()
-    print("SRR light: {0} Ranges: {1}".format(SRR.light, SRR.ranges))
 
     OI.Get_All_Sensors()
-    print("Bumps L R: {0} {1}".format(OI.Bump_Left(), OI.Bump_Right()))
+    bump_left  = OI.Bump_Left()
+    bump_right = OI.Bump_Right()
+
+    print("SRL light: {0}\tRanges: {1}".format(SRL.light, SRL.ranges))
+    print("SRR light: {0}\tRanges: {1}".format(SRR.light, SRR.ranges))
+    print("Bumps L R: {0} {1}".format(bump_left, bump_right))
 
 if __name__ == "__main__":
     CI  = Communication_Interfaces()
 
-    OI  = Open_Interface(CI.uart_read, CI.uart_write)
+    OI  = Open_Interface(CI.uart_write, CI.uart_read)
     OI.Start()
     OI.Full()
 
     SRL = SRF08(CI.i2c_read, CI.i2c_write, 0x72)
-    SRL.set_gain(31)
-    SRL.set_range(255)
+    SRL.set_gain(0)
+    SRL.set_range(140)
 
     SRR = SRF08(CI.i2c_read, CI.i2c_write, 0x76)
-    SRR.set_gain(31)
-    SRR.set_range(255)
+    SRR.set_gain(0)
+    SRR.set_range(140)
 
+    os.system('clear')
     while (True):
         robot_status(OI, SRL, SRR)
         sleep(1)
